c8a0d36d2687e9ce70791a010bb9c148a0bd2320
Carfax-Marci <marcel.westenberg@hotmail.de>
2021-10-13 18:03:18 +0200

    tried to fix the "c# bug"

---
+++ AOC-2020-CSharp/AOC_2020_Day_1_Report_Repair/AOC_2020_Day_1_Report_Repair/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AOC_2020_Day_1_Report_Repair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
